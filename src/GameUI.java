114f5dac37d7b07e257e3109783f1b98176ee1e7
Christopher Siu <christopher@get2thecrossing.com>
2016-10-09 14:03:07 -0700

    Added skeletons for SmartRochambeau and GameUI.

---
+++ src/GameUI.java
@@ -0,0 +1,12 @@
+/**
+ * Defines a common interface for UI classes.
+ * @author cesiu
+ */
+
+public interface GameUI
+{
+   /**
+    * Instructs the UI to display the most recent results.
+    */
+   public void display();
+}
