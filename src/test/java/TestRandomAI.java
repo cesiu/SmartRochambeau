6ac1f9746bcc55ec971ce7328138587b6baebf2c
Christopher Siu <christopher@get2thecrossing.com>
2016-10-19 20:46:29 -0700

    Removed instance variable from tests.

--- src/test/java/TestRandomAI.java
+++ src/test/java/TestRandomAI.java
@@ -1,49 +1,50 @@
 /**
  * Contains unit tests for RandomAI.
- * @author cesiu
  * @author cbrown83
+ * @author cesiu
  * @version October 19, 2016
  */
 
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class TestRandomAI {
-	
-	private RandomAI randomAI = new RandomAI(); 
-	
-	@Test 
-	public void testEmptyConstructor() {
-		assertTrue("Testing RandomAI empty constructor...", 
-					randomAI instanceof RandomAI);
-	}
-	
-	@Test
-	public void testNonEmptyConstructor() {
-		randomAI = new RandomAI(3);
-		assertTrue("Testing RandomAI non-empty constructor...",
-					randomAI instanceof RandomAI); 
-	}
-	
-	
-	@Test
-	public void testMakeThrow() {
-		assertTrue("Testing make throw...", 
-					randomAI.makeThrow() instanceof GameModerator.GameThrow);
-	}
-	
-	@Test 
-	public void testStoreResult() {
-		int result = 0; 
-		randomAI.storeResult(randomAI.makeThrow(), result);
-		// function should do nothing
-		assertEquals("Testing storeResult for RandomAI does nothing...", 
-					0, result);
-	}
-	
-	@Test
-	public void testImplementsGameAI() {
-		assertTrue("Testing RandomAI implements GameAI...",
-					randomAI instanceof GameAI); 
-	}
+public class TestRandomAI
+{   
+   @Test 
+   public void testDefaultConstructor() {
+      RandomAI randomAI = new RandomAI(); 
+      assertTrue("Testing RandomAI default constructor...", 
+                 randomAI instanceof RandomAI);
+   }
+   
+   @Test
+   public void testSeededConstructor() {
+      RandomAI randomAI = new RandomAI(3);
+      assertTrue("Testing RandomAI seeded constructor...",
+                 randomAI instanceof RandomAI); 
+   }
+   
+   @Test
+   public void testMakeThrow() {
+      RandomAI randomAI = new RandomAI(); 
+      assertTrue("Testing make throw...", randomAI.makeThrow() 
+                 instanceof GameModerator.GameThrow);
+   }
+   
+   @Test 
+   public void testStoreResult() {
+      RandomAI randomAI = new RandomAI(); 
+      int result = 0; 
+      randomAI.storeResult(randomAI.makeThrow(), result);
+      // function should do nothing
+      assertEquals("Testing storeResult for RandomAI does nothing...", 
+                   0, result);
+   }
+   
+   @Test
+   public void testImplementsGameAI() {
+      RandomAI randomAI = new RandomAI(); 
+      assertTrue("Testing RandomAI implements GameAI...",
+                 randomAI instanceof GameAI); 
+   }
 }
